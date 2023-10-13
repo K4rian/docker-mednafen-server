6305e6222ce1a9366bd6847e09c47f2762729253
K4rian <contact@k4rian.com>
2023-10-13 17:31:36 +0200

    Update the startup script by make it simpler, remove bash dependency and add support for dynamic password in config file

--- container_files/docker-entrypoint.sh
+++ container_files/docker-entrypoint.sh
@@ -1,105 +1,63 @@
-#!/bin/bash
+#!/bin/ash
+# Mednafen-Server Startup Script
+#
+# Server Files: /home/mednafen
 
-declare -r MDFNSV_CONFFILE_DEFAULT="$HOME/server.conf.default"
+export LD_LIBRARY_PATH=$HOME/lib:$LD_LIBRARY_PATH
 
-if [ ! -f "$MDFNSV_CONFFILE_DEFAULT" ]; then
-    printf "[ERROR]Default config file not found.\n"
-    exit 1
+clear
+
+readonly s_config_file=$HOME/server.conf
+
+echo "░█▄█░█▀▀░█▀▄░█▀█░█▀█░█▀▀░█▀▀░█▀█░░░░░█▀▀░█▀▀░█▀▄░█░█░█▀▀░█▀▄"
+echo "░█░█░█▀▀░█░█░█░█░█▀█░█▀▀░█▀▀░█░█░▄▄▄░▀▀█░█▀▀░█▀▄░▀▄▀░█▀▀░█▀▄"
+echo "░▀░▀░▀▀▀░▀▀░░▀░▀░▀░▀░▀░░░▀▀▀░▀░▀░░░░░▀▀▀░▀▀▀░▀░▀░░▀░░▀▀▀░▀░▀"
+
+# Generates the config file if it doesn't exists
+if [ ! -f "$s_config_file" ]; then
+  echo "Generating new configuration file: ${s_config_file}"
+  printf "%s %i\n%s %i\n%s %i\n%s %i\n%s %i\n%s %i\n%s %i\n" \
+    "maxclients" ${MDFNSV_MAXCLIENTS} \
+    "connecttimeout" ${MDFNSV_CONNECTTIMEOUT} \
+    "port" ${MDFNSV_PORT} \
+    "idletimeout" ${MDFNSV_IDLETIMEOUT} \
+    "maxcmdpayload" ${MDFNSV_MAXCMDPAYLOAD} \
+    "minsendqsize" ${MDFNSV_MINSENDQSIZE} \
+    "maxsendqsize" ${MDFNSV_MAXSENDQSIZE} \
+  >$s_config_file
+else
+  echo "Using existing configuration file: ${s_config_file}"
 fi
 
-declare -r MDFNSV_CONFFILE="$HOME/server.conf"
+if [ ! "x$MDFNSV_ISPUBLIC" = "x" ] \
+ && [ "$MDFNSV_ISPUBLIC" = 1 ]; then
+  # Removes the line containing the password if present
+  # in the config file
+  if ( grep -q 'password' $s_config_file ); then
+    sed -i '/password /d' $s_config_file
+  fi
+else
+  # Sets the password if given
+  # The environment variable take precedence over the docker secret
+  s_password="${MDFNSV_PASSWORD}"
 
-if [ ! -f "$MDFNSV_CONFFILE" ]; then
-    cp $MDFNSV_CONFFILE_DEFAULT $MDFNSV_CONFFILE
+  if [ "x$s_password" = "x" ] \
+   && [ -f "/run/secrets/mednafenserver" ]; then
+    s_password=$(cat "/run/secrets/mednafenserver")
+  fi
 
-    # Read the configuration file and declare all properties as global conf variables
-    while read -r line; do
-        if [[ ! ${line::1} =~ ^[[:alnum:]]+$ ]]
-        then
-            continue
-        fi
-
-        a_line=($(echo $line | tr $"\t" "\n"))
-        s_key="${a_line[0]}"
-        s_value="${a_line[1]}"
-
-        if [[ $s_value == *";"* ]]; then
-            a_value=($(echo $s_value | tr ";" "\n"))
-            s_value="${a_value[0]}"
-        fi
-
-        printf -v "$s_key" "%s" "$s_value"
-    done < $MDFNSV_CONFFILE
-
-    if [ -z ${password+x} ]; then
-        declare -g "password"
+  # Replaces or appends the password in the config file
+  if [ ! "x$s_password" = "x" ]; then
+    if ( grep -q 'password' $s_config_file ); then
+      sed -i "/password /c\password ${s_password}" $s_config_file
+      echo "Password updated in the configuration file"
+    else
+      echo "password ${s_password}" >> $s_config_file
+      echo "Password added in the configuration file"
     fi
-
-    # Update the password variable if the corresponding docker secret has been given
-    if [ -f "/run/secrets/mednafenserver" ]; then
-        password=$(<"/run/secrets/mednafenserver")
-    fi
-
-    # Update the conf variables with the env variables values
-    if [ ! -z ${MDFNSV_MAXCLIENTS+x} ]; then
-        maxclients=$MDFNSV_MAXCLIENTS
-        unset MDFNSV_MAXCLIENTS
-    fi
-
-    if [ ! -z ${MDFNSV_CONNECTTIMEOUT+x} ]; then
-        connecttimeout=$MDFNSV_CONNECTTIMEOUT
-        unset MDFNSV_CONNECTTIMEOUT
-    fi
-
-    if [ ! -z ${MDFNSV_PORT+x} ]; then
-        port=$MDFNSV_PORT
-        unset MDFNSV_PORT
-    fi
-
-    if [ ! -z ${MDFNSV_IDLETIMEOUT+x} ]; then
-        idletimeout=$MDFNSV_IDLETIMEOUT
-        unset MDFNSV_IDLETIMEOUT
-    fi
-
-    if [ ! -z ${MDFNSV_MAXCMDPAYLOAD+x} ]; then
-        maxcmdpayload=$MDFNSV_MAXCMDPAYLOAD
-        unset MDFNSV_MAXCMDPAYLOAD
-    fi
-
-    if [ ! -z ${MDFNSV_MINSENDQSIZE+x} ]; then
-        minsendqsize=$MDFNSV_MINSENDQSIZE
-        unset MDFNSV_MINSENDQSIZE
-    fi
-
-    if [ ! -z ${MDFNSV_MAXSENDQSIZE+x} ]; then
-        maxsendqsize=$MDFNSV_MAXSENDQSIZE
-        unset MDFNSV_MAXSENDQSIZE
-    fi
-
-    # Takes precedence over the docker secret
-    if [ ! -z ${MDFNSV_PASSWORD+x} ]; then
-        password=$MDFNSV_PASSWORD
-        unset MDFNSV_PASSWORD
-    fi
-
-    # Save all conf variables to the server configuration file
-    printf -v current_date '%(%Y-%m-%d|%T)T' -1
-    printf "; %s\n%s %i\n%s %i\n%s %i\n%s %i\n%s %i\n%s %i\n%s %i" \
-      $current_date \
-      "maxclients" $maxclients \
-      "connecttimeout" $connecttimeout \
-      "port" $port \
-      "idletimeout" $idletimeout \
-      "maxcmdpayload" $maxcmdpayload \
-      "minsendqsize" $minsendqsize \
-      "maxsendqsize" $maxsendqsize \
-    >$MDFNSV_CONFFILE
-
-    if [[ $password ]]; then
-        printf "\npassword %s" $password >>$MDFNSV_CONFFILE
-        unset $password
-    fi
+    unset MDFNSV_PASSWORD
+    unset s_password
+  fi
 fi
 
-# Start the server
-$HOME/mednafen-server $MDFNSV_CONFFILE
+$HOME/mednafen-server $s_config_file
